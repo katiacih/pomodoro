494b18f669a25b0bb6ebf533dd043d54264f8376
katiacih <katiacih@gmail.com>
2021-05-16 18:19:34 -0300

    refactor: tasks

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../components/card';
+import Button from '../components/button'
+import './tasks.css'
  
 const Tasks: React.FC = () => {
+  const [newTask, setNewTask] = useState('')
+
+  const onChangeNewTask = (evt: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewTask(evt.target.value)
+  }
+
+  const addNewTask = (): void => {
+    
+  }
+
   return (
-    <div>Tasks
-    </div>
+    <Card>
+      <div className='header'>
+        <h2>Tasks</h2>
+        <p>Inclua suas tarefas para melhorar aproveitamento.</p>
+
+      </div>
+
+      <input value={newTask} onChange={ evt => onChangeNewTask(evt)}/>
+      <div className="addtasks">
+        <Button label="Add Task" onClick={() => addNewTask()}
+          />
+      </div>
+
+    </Card>
   );
 }
 export default Tasks;
