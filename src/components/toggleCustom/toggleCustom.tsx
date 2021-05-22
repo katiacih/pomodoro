55d2e8d17a24f762c62c0e4002fe4cc7f3565829
katiacih <katiacih@gmail.com>
2021-05-21 21:06:51 -0300

    feat: incluido campo para definir tema

--- src/components/toggleCustom/toggleCustom.tsx
+++ src/components/toggleCustom/toggleCustom.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import { BsMoon, BsSun } from 'react-icons/bs'
 import './toggleCustom.css';
 
 type Props = {
   value: boolean
   disabled?: boolean
   onChange: (value: boolean) => void
-
 }
 
 const ToggleCustom: React.FC<Props> = ({ value, disabled = false, onChange}: Props) => {
   return (
     <div className="toggleContent">
-      <label>Theme Dark</label>
+      <span className="btnIcon" onClick={() => onChange(false)}><BsMoon className='moon'/></span>
       <input
         onClick={() => onChange(!value)}
         disabled={disabled}
@@ -19,6 +19,7 @@ const ToggleCustom: React.FC<Props> = ({ value, disabled = false, onChange}: Pro
         type="checkbox"
         name="checkbox"
         className="toggle" />
+      <span className="btnIcon" onClick={() => onChange(true)}><BsSun className='sun'/></span>
     </div>
   );
 }
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -2,9 +2,30 @@ import React, { useState } from 'react';
 import Card from '../components/card';
 import Button from '../components/button'
 import './tasks.css'
+
+type TaskProps = {
+  descricao: string
+  isCompleted: boolean
+}
+
+const t = [
+  {
+    descricao: 'Corrigir exercicios',
+    isCompleted: false
+  },
+  {
+    descricao: 'Estudar javascript',
+    isCompleted: false
+  },
+  {
+    descricao: 'Criar banners',
+    isCompleted: false
+  }
+]
  
 const Tasks: React.FC = () => {
   const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState<TaskProps[]>(t)
 
   const onChangeNewTask = (evt: React.ChangeEvent<HTMLInputElement>): void => {
     setNewTask(evt.target.value)
@@ -12,6 +33,10 @@ const Tasks: React.FC = () => {
 
   const addNewTask = (): void => {
     
+  }
+
+  const getTasksFinalizadas = (): number => {
+    return 1;
   }
 
   return (
@@ -26,6 +51,19 @@ const Tasks: React.FC = () => {
       <div className="addtasks">
         <Button label="Add Task" onClick={() => addNewTask()}
           />
+      </div>
+
+      <div className='listaTasks'>
+        <h3>Tasks Finalizadas: {getTasksFinalizadas()}</h3>
+
+        <ul>
+          { tasks.map((item) => 
+             <li>{item.descricao}</li>
+            )
+            }
+        </ul>
+
+
       </div>
 
     </Card>
